f7b061fe3404f066b3bd580c6d7d0179bc47cf99
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-05 20:14:19 -0500

    BiFunction con 3 tipos de dato

--- JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_04_functional/StringFunctions.java
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_04_functional/StringFunctions.java
@@ -1,5 +1,7 @@
 package com.platzi.functional._04_functional;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class StringFunctions {
@@ -8,5 +10,7 @@ public class StringFunctions {
         UnaryOperator<String> addMark = text -> text + "!";
         System.out.println(quote.apply("Corrobar resultado"));
         System.out.println(addMark.apply("Hello"));
+        BiFunction<Integer, Integer, Integer> multiplicacion = (x,y) -> x*y;
+        System.out.println(multiplicacion.apply(5,4));
     }
 }
