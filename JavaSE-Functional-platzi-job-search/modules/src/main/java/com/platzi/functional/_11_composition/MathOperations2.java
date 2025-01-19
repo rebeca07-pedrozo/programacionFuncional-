c616fe33dc87a966c62b6954d6f61a8e6751390c
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-19 16:56:13 -0500

    mutiply by 3

---
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
@@ -0,0 +1,9 @@
+package com.platzi.functional._11_composition;
+
+import java.util.function.Function;
+
+public class MathOperations2 {
+    public static void main(String[] args) {
+        Function<Integer, Integer> multiplyBy3= x-> x*3;
+    }
+}
