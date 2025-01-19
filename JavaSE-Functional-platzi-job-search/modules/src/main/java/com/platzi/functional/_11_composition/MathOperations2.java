dc459bba413cba8a6a766d5a4579478569b38177
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-19 17:04:45 -0500

    Usando el compose para ejecutar primero la landa y luego el multiply by 3

--- JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
@@ -4,6 +4,18 @@ import java.util.function.Function;
 
 public class MathOperations2 {
     public static void main(String[] args) {
-        Function<Integer, Integer> multiplyBy3= x-> x*3;
+        Function<Integer, Integer> multiplyBy3= x-> {
+            System.out.println("Multiplicando x:"+ x+ "por 3");;
+            return x*3;
+        };
+
+        Function< Integer, Integer> addMultiplyBy3=
+                multiplyBy3.compose(y-> {
+                    System.out.println("Agrege 1 a: "+y);
+                    return y+1;
+                });
+        System.out.println(
+                addMultiplyBy3.apply(5)
+        );
     }
 }
