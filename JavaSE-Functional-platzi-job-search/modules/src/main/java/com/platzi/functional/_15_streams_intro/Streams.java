9c410857b13cf37bc5bca15a2aa9f100702422c3
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-19 19:37:27 -0500

    Stream peek

--- JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/Streams.java
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/Streams.java
@@ -1,7 +1,8 @@
 package com.platzi.functional._15_streams_intro;
+
 import com.platzi.functional._06_reference_operator.NombresUtils;
+
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Streams {
@@ -10,19 +11,36 @@ public class Streams {
                 "Java",
                 "FrontEnd",
                 "Backend",
-                 "FullStack");
-        for(String course: courseList){
+                "FullStack"
+        );
+
+        for (String course : courseList) {
             String newCourseName = course.toLowerCase().replace("!", "!!");
             System.out.println("Platzi: " + newCourseName);
         }
-        Stream<String> coursesStream = Stream.of("Java",
+
+        Stream<String> coursesStream = Stream.of(
+                "Java",
                 "FrontEnd",
                 "Backend",
-                "FullStack");
-        //Stream<Integer>courseLenghtStream = coursesStream.map(course -> course.length());
-        //Optional<Integer> longest = courseLenghtStream.max((x, y) -> y-x);
-        Stream<String> emphasisCourses = coursesStream.map(course -> course + "!");
-        Stream<String> justJavaCourses = emphasisCourses.filter(course -> course.contains("java"));
-        justJavaCourses.forEach(System.out::println);
+                "FullStack"
+        );
+
+        coursesStream
+                .map(course -> course + "!")
+                .filter(course -> course.toLowerCase().contains("java"))
+                .forEach(System.out::println);
+
+        Stream<String> coursesStream2 = courseList.stream();
+
+        addOperator(
+                coursesStream2
+                        .map(course -> course + "!!")
+                        .filter(course -> course.toLowerCase().contains("java")) // Filtro insensible a mayúsculas
+        ).forEach(System.out::println);
+    }
+
+        static <T> Stream<T> addOperator(Stream<T> stream) {
+        return stream.peek(data -> System.out.println("Dato: " + data));
     }
 }
