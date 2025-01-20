19a324e1dc1077ebe70116a8b2f29777668046d5
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-19 19:54:02 -0500

    generador de datos

---
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -0,0 +1,12 @@
+package com.platzi.functional._15_streams_intro;
+
+import java.util.stream.IntStream;
+
+public class TypeStream {
+    public static void main(String[] args) {
+        IntStream infiniteStream = IntStream.iterate(0, x -> x+1);
+        infiniteStream.limit(1000)
+                .filter(x-> x%2 ==0)
+                .forEach(System.out::println);
+    }
+}
