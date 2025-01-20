25c0c880820e32ba6514a903468dc33c929cd9fa
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-01-19 20:28:54 -0500

    Collectors

--- JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ JavaSE-Functional-platzi-job-search/modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,12 +1,15 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x+1);
-        infiniteStream.limit(1000)
+        List<Integer> numbersList =infiniteStream.limit(1000)
                 .filter(x-> x%2 ==0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
